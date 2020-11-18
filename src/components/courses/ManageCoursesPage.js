0f2a85ee517c3ab061d42cf6662e1047883e75b0
anandsun78 <anandsun78@gmail.com>
2020-11-17 20:07:25 -0700

    Added loading and toast message

--- src/components/courses/ManageCoursesPage.js
+++ src/components/courses/ManageCoursesPage.js
@@ -5,6 +5,8 @@ import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
+import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 
 function ManageCoursePage({
   courses,
@@ -17,6 +19,8 @@ function ManageCoursePage({
 }) {
   const [course, setCourse] = useState(props.course);
   const [errors, setErrors] = useState({});
+  const [saving, setSaving] = useState(false);
+
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
@@ -43,17 +47,22 @@ function ManageCoursePage({
 
   function handleSave(event) {
     event.preventDefault();
+    setSaving(true);
     saveCourse(course).then(() => {
+      toast.success("Course Saved");
       history.push("/courses");
     });
   }
-  return (
+  return authors.length === 0 || courses.length === 0 ? (
+    <Spinner />
+  ) : (
     <CourseForm
       course={course}
       errors={errors}
       authors={authors}
       onChange={handleChange}
       onSave={handleSave}
+      saving={saving}
     />
   );
 }
